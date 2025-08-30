a83ab8bfc91dec9c56fa20fda3d3e4d2a99910e8
leonardo <leonardo.rubert@edu.pucrs.br>
2025-08-30 18:46:07 -0300

    auto-update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1344329299"
+    serviceWorkerVersion: "1804580091"
   }
 });
